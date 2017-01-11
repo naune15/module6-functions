225dfb53c7bb89a893d178cf421c9b57b65a8ba6
iGuest <iGuest>
2017-01-11 14:20:25 -0800

    Completed exercise 1 and 2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,15 +2,20 @@
 
 # Define a function `AddThree` that takes a single argument and
 # returns a value 3 greater than that input
-
+AddThree <- function(input) {
+  return(input + 3)
+}
 
 # Create a variable `ten` that is the result of passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Define a function `ImperialToMetric` that takes in two arguments: a number of feet
 # and a number of inches
 # The function should return the total length in meters
-
+ImperialToMetric <- function(feet, inches) {
+  return(0.0252 * (inches + 12*feet))
+}
 
 # Create a variable `height.in.meters` by passing your height in imperial to the
 # `ImperialToMetric` function
+height.in.meters <- ImperialToMetric(5, 11)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,14 +3,26 @@
 # Define a function `CompareLength` that takes in 2 character strings, and returns
 # the sentence of the form
 #   "The difference in length is N"
-
+CompareLength <- function(str1, str2) {
+  difference <- abs(nchar(str1) - nchar(str2))
+  return(paste("The difference in length is", difference))
+}
 
 # Pass two strings of different lengths to your `CompareLength` function
-
+CompareLength("right", "left")
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
-
+DescribeDifference <- function(str1, str2) {
+  difference <- abs(nchar(str1) - nchar(str2))
+  if(nchar(str1) > nchar(str2)) {
+    return(paste("Your first string is longer by", difference, "characters"))
+  }
+  else {
+    return(paste("Your second string is longer by", difference, "characters"))
+  }
+}
 
 # Pass two strings of different lengths to your `DescribeDifference` function
+DescribeDifference("rainbows", "butterflies")
